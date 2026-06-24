03efb45b8a2399fca1311627e125bf69a19b4a8b
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-06-24 15:22:22 +0530

    maths for DSA

--- Collection/Coillection.java
+++ Collection/Coillection.java
@@ -1,7 +1,12 @@
 package Collection;
 
+import java.util.Scanner;
+
 public class Coillection {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
        /*
           /*
 
@@ -40,6 +45,105 @@ public class Coillection {
         */
 
         // https://leetcode.com/problems/spiral-matrix-ii/description/
-    }
 
+        /*
+        class Solution {
+    public int[][] generateMatrix(int n) {
+        // int n = mat.length; int m = mat[0].length;
+        int [][] ans = new int[n][n];
+        int i = 0 , j = 0 , k = 0;
+        int [][] dir = {{0 , 1} , {1 , 0} , {0 , -1} , {-1 , 0}};
+
+        // ArrayList<Integer> ans = new ArrayList<>();
+        int cnt = 0;
+
+        while(true) {
+            ans[i][j] = ++cnt;
+            // ans.add(mat[i][j]);
+
+            int ix = i , jx = j;
+            ix += dir[k][0]; jx += dir[k][1]; // next coordn
+
+            if(ix >= n || jx >= n || ix < 0 || jx < 0 || (ans[ix][jx] != 0)) {
+                ix = i; jx = j;
+                k = (k + 1) % 4;
+
+                ix += dir[k][0]; jx += dir[k][1];
+
+                if(ix >= n || jx >= n || ix < 0 || jx < 0 || (ans[ix][jx] != 0))
+                   break;
+            }
+
+            i = ix; j = jx;
+        }
+
+        return ans;
+    }
+}
+         */
+
+        // sqrt
+
+        long n = sc.nextLong();
+
+//        for(long i = 1; i <= n; ++i)
+//            if(i*i == n) {
+//                System.out.println(i + " is sqrt of " + n);
+//                return;
+//            } // O(N) --> 10^8
+//        System.out.println("No sqrt for " + n);
+
+/*
+        int cnt = 0;
+
+        for(long i = 1; i <= n; ++i)
+            if(i*i == n) {
+                System.out.println(cnt);
+                System.out.println(i + " is sqrt of " + n);
+                break;
+            } else if(i*i > n) {
+                System.out.println(cnt);
+                System.out.println("No sqrt for " + n);
+                break;
+            } else {
+                ++cnt;
+            }  // O(sqrt(N))
+
+            long lo = 1 , hi = n;
+
+        System.out.println("))))))))))))))");
+        long cnt1 = 0;
+
+           while(lo <= hi) {
+               long mid = (lo + hi) / (long)2;
+
+               if(mid * mid == n) {
+                   System.out.println(cnt1);
+                   System.out.println(mid + " is sqrt of " + n);
+                   return;
+
+               } else if(mid * mid > n) {
+                   hi = mid - 1;
+                   ++cnt1;
+               } else if(mid * mid < n) {
+                   lo = mid + 1;
+                   ++cnt1;
+               }
+           } // O(log(N))
+
+        System.out.println(cnt1 + " : cnt1");
+
+        System.out.println("No sqrt for " + n);
+
+ */
+        for(long i = 2; i*i <= n; ++i)
+            if(n % i == 0) {
+                System.out.println(n + " is composite");
+                return;
+            }
+
+        System.out.println(n + " is a prime");
+
+
+    }
 }

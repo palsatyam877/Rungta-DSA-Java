461a12802ede3a50b1d5d789e04c72440ca07c77
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-06-24 10:41:09 +0530

    Lec-2 - pattern

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+         // https://leetcode.com/problems/spiral-matrix/
+        // https://leetcode.com/problems/spiral-matrix-ii/description/
     }
 }

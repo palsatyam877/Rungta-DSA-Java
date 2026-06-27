b354c419a954d79410b68fb18d06fe1f0f604016
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-06-27 09:33:08 +0530

    Sets in java

--- Collection/JavaColllections.java
+++ Collection/JavaColllections.java
@@ -1,5 +1,4 @@
 package Collection;
-
 import java.util.*;
 
 public class JavaColllections {
@@ -12,8 +11,6 @@ public class JavaColllections {
     }
 
     public static void main(String[] args) {
-
-        System.out.println(f(5));
 
         // ArrayList
 
@@ -71,7 +68,7 @@ public class JavaColllections {
 
  */
 
-        ArrayList<Integer> arr = new ArrayList<>(List.of(1 , 3 , 5));
+//        ArrayList<Integer> arr = new ArrayList<>(List.of(1 , 3 , 5));
 //        System.out.println(arr);
 
 //        Iterator<Integer> i = arr.iterator();
@@ -90,30 +87,30 @@ public class JavaColllections {
         // System.out.println(w.get(3).get(2).get(3).get(4)); --> 81
 
         // w.get(5)
-        ArrayList<Integer> a1 = new ArrayList<>(List.of(0 , 0 , 0 , 0 , 0 , 7));
-        a1.set(3 , 12);
-        a1.set(4 , 81);
+//        ArrayList<Integer> a1 = new ArrayList<>(List.of(0 , 0 , 0 , 0 , 0 , 7));
+//        a1.set(3 , 12);
+//        a1.set(4 , 81);
 
 //        System.out.println(a1.get(5));
 
-        ArrayList<Integer> e1 = new ArrayList<>();
+//        ArrayList<Integer> e1 = new ArrayList<>();
 
         // .get(7).get(5)
-        ArrayList<ArrayList<Integer>> a2 = new ArrayList<>(List.of(e1,e1,e1,a1,e1,e1,e1,a1 , e1 , a1));
+//        ArrayList<ArrayList<Integer>> a2 = new ArrayList<>(List.of(e1,e1,e1,a1,e1,e1,e1,a1 , e1 , a1));
 
 //        System.out.println(a2.get(7).get(5));
 
-        ArrayList<ArrayList<Integer>> e2 = new ArrayList<>();
-        ArrayList<ArrayList<ArrayList<Integer>>> a3 = new ArrayList<>(List.of(e2 , e2 , a2 , e2 , e2 , e2 , a2));
+//        ArrayList<ArrayList<Integer>> e2 = new ArrayList<>();
+//        ArrayList<ArrayList<ArrayList<Integer>>> a3 = new ArrayList<>(List.of(e2 , e2 , a2 , e2 , e2 , e2 , a2));
 
 //        System.out.println(a3.get(2).get(7).get(5));
 
-        ArrayList<ArrayList<ArrayList<Integer>>> e3 = new ArrayList<>();
-        ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> a4 = new ArrayList<>(List.of(e3 , a3 , e3 , a3));
-
-         System.out.println(a4.get(3).get(2).get(7).get(5));
-          System.out.println(a4.get(1).get(6).get(9).get(3));
-        System.out.println(a4.get(3).get(2).get(3).get(4));
+//        ArrayList<ArrayList<ArrayList<Integer>>> e3 = new ArrayList<>();
+//        ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> a4 = new ArrayList<>(List.of(e3 , a3 , e3 , a3));
+//
+//         System.out.println(a4.get(3).get(2).get(7).get(5));
+//          System.out.println(a4.get(1).get(6).get(9).get(3));
+//        System.out.println(a4.get(3).get(2).get(3).get(4));
         //  System.out.println(w.get(1).get(6).get(9).get(3)); --> 12
         // System.out.println(w.get(3).get(2).get(3).get(4)); --> 81
 
@@ -226,6 +223,25 @@ class Solution {
     }
 }
      */
-    }
 
+        // Set
+
+        /*
+            HashSet<>
+            LinkedHashset<>
+            TreeHashSet<>
+        */
+
+        // https://www.geeksforgeeks.org/problems/minimum-vertical-sum-1593518814/1
+        // https://www.geeksforgeeks.org/problems/union-of-two-arrays3538/1
+        // https://www.geeksforgeeks.org/problems/intersection-of-two-arrays2404/1
+        // https://www.geeksforgeeks.org/problems/distinct-sorted--124713/1
+
+        // System.out.println(lhs5.get(3).getLast().getFirst().get(2).getFirst()); // Number Theory
+        // System.out.println(lhs5.get(3).getLast().getLast().get(4).getLast()); // Recursion
+
+        // Iterator --> Number Theory
+
+        // Iterator --> Recursion
+    }
 }

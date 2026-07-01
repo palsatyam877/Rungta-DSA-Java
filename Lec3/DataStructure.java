4735fee781a46739408b15fe44e66a1d029262e4
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-01 10:35:58 +0530

    Data Structure / Collection -> final

--- Lec3/DataStructure.java
+++ Lec3/DataStructure.java
@@ -3,6 +3,54 @@ import java.util.*;
 
 // https://leetcode.com/problems/two-sum/
 // https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/
+
+/*
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+
+        int count = 0;
+
+        while(nums.length > 0) {
+            int n = nums.length;
+            int mn = Integer.MAX_VALUE;
+            int inx = n - 1;
+            boolean isSort = true;
+
+            for(int i = 0; i < n - 1; ++i)
+                 if(nums[i] > nums[i + 1])
+                    isSort = false;
+
+            if(isSort)
+                return count;
+
+            for(int i = n - 2; i >= 0; --i)
+                if(nums[i] + nums[i + 1] <= mn) {
+                    mn = nums[i] + nums[i + 1];
+                    inx = i;
+                }
+
+            int [] temp = new int[n - 1];
+            int ix = 0;
+
+            for(int i = 0; i < n; ++i)
+               if(i == inx) {
+                   temp[ix++] = mn;
+               } else if(i == inx + 1) {
+
+               } else {
+                    temp[ix++] = nums[i];
+               }
+
+            nums = temp;
+            ++count;
+        }
+
+        return count;
+    }
+}
+
+*/
+
 // https://leetcode.com/problems/happy-number/
 // https://leetcode.com/problems/contains-duplicate-ii/
 // https://leetcode.com/problems/merge-similar-items/

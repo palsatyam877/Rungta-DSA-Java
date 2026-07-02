5b3a995c32232f7fd87237501a5877a0c4e62e05
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-02 10:26:25 +0530

    Stacks - Lec -> 4

--- Lec4/StackDataStructure.java
+++ Lec4/StackDataStructure.java
@@ -8,6 +8,36 @@ import java.util.*;
 
 // https://leetcode.com/problems/merge-similar-items/
 // https://leetcode.com/problems/find-right-interval/
+
+/*
+class Solution {
+    public int[] findRightInterval(int[][] intv) {
+        int n = intv.length;
+
+        int [] ans = new int[n];
+        TreeMap<Integer , Integer> mp = new TreeMap<>();
+
+        for(int i = 0; i < n; ++i) {
+            int start = intv[i][0] , end = intv[i][1];
+            mp.put(start , i);
+        }
+
+        for(int i = 0; i < n; ++i) {
+            int start = intv[i][0] , end = intv[i][1];
+            ans[i] = -1;
+
+            if(mp.ceilingKey(end) != null) {
+                int nxtStart = mp.ceilingKey(end);
+                ans[i] = mp.get(nxtStart);
+            }
+        }
+
+        return ans;
+    }
+}
+
+*/
+
 // https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
 // https://www.geeksforgeeks.org/problems/immediate-smaller-element1142/1
 // https://www.geeksforgeeks.org/problems/previous-greater-element/1
@@ -23,10 +53,10 @@ public class StackDataStructure {
        stk.add(2);
        stk.add(1);
 
-       System.out.println(stk);
-       System.out.println(stk.peek());
+//       System.out.println(stk);
+//       System.out.println(stk.peek());
        stk.pop();
-       System.out.println(stk);
+//       System.out.println(stk);
 
        ArrayList<Integer> arrL = new ArrayList<>();
        arrL.add(59);
@@ -36,8 +66,26 @@ public class StackDataStructure {
        arrL.add(25);
 
        arrL.set(2 , 4);
-        System.out.println(arrL);
+//        System.out.println(arrL);
 
-      TreeMap<Integer , Integer> mp = new TreeMap<>();
+       TreeSet<Integer> ts = new TreeSet<>();
+       ts.add(-1 * 4);
+       ts.add(-1 * 10);
+       ts.add(-1 * 15);
+       ts.add(-1 * 16);
+       ts.add(-1 * 17);
+       ts.add(-1 * 21);
+       ts.add(-1 * 25);
+
+//       System.out.println(-ts.ceiling(-20));
+
+       TreeMap<Integer , Integer> mp = new TreeMap<>();
+       mp.put(3 , 10);
+       mp.put(5 , 15);
+       mp.put(10 , 19);
+
+       System.out.println(mp.ceilingKey(5));
+
+
     }
 }

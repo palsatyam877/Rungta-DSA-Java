87e56dd0df6c32146b8dd7f88ef7820ff1e8ce0b
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-03 09:13:18 +0530

    Stacks - Lec -> 4

---
+++ Lec5/StackDataStructureContinue.java
@@ -0,0 +1,19 @@
+package Lec5;
+import java.util.*;
+
+public class StackDataStructureContinue {
+
+    // https://www.geeksforgeeks.org/problems/reverse-a-string-using-stack/1
+    // https://leetcode.com/problems/largest-rectangle-in-histogram/
+    // https://leetcode.com/problems/trapping-rain-water/
+
+    /******************** Sliding Windows && Two Pointers *****************************/
+
+    // https://leetcode.com/problems/product-of-array-except-self/
+    // https://leetcode.com/problems/maximum-subarray/
+    // https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+
+    public static void main(String [] args) {
+        System.out.println("Jai Maa Jagat Janani");
+    }
+}

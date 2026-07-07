7a76a3d287cbccac6719b4dee8da58e63d8e2049
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-07 11:00:15 +0530

    Two Pointers and Sliding Window

--- Lec8/SlidingWindowTwoPointerAdvance.java
+++ Lec8/SlidingWindowTwoPointerAdvance.java
@@ -11,6 +11,46 @@ import java.util.*;
 
 // https://leetcode.com/problems/sum-of-all-odd-length-subarrays/   // O(N)
 
+/*
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int n = arr.length;
+        int odd = 0 , even = 0 , pfsum = 0 , cntO = 0 , cntE = 0;
+
+        for(int i = 0; i < n; ++i) {
+            pfsum += arr[i];
+
+            if((i % 2) != 0) {
+                even += pfsum;
+                cntE++;
+            } else {
+                odd += pfsum;
+                cntO++;
+            }
+        }
+
+        int cO = (n % 2) != 0 ? (n / 2) + 1 : (n / 2) , cE = n / 2;
+        int ans = 0;
+
+        for(int i = 0; i < n; ++i) {
+            if(i % 2 == 0)
+                ans += odd;
+            else
+                ans += even;
+
+            odd -=(cO * arr[i]);
+            even -= (cE * arr[i]);
+
+            if(i % 2 == 0) cO--;
+            else cE--;
+        }
+
+        return ans;
+    }
+}
+
+*/
+
 // https://leetcode.com/problems/sliding-window-maximum/
 
 // https://leetcode.com/problems/minimum-window-substring/
@@ -21,8 +61,7 @@ import java.util.*;
 
 public class SlidingWindowTwoPointerAdvance {
 
-
     public static void main(String [] args) {
-
+        System.out.println("Lec 8");
     }
 }

9d47eb9364066ff3d97efda1b46c82de69bd1287
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-08 23:59:14 +0530

    BINARY SEARCH ON ANSWER

--- Lecz10/BinarySearchOnAnswer.java
+++ Lecz10/BinarySearchOnAnswer.java
@@ -1,13 +1,14 @@
 package Lecz10;
 
 // https://leetcode.com/problems/sliding-window-median/
+
+/************************ BINARY SEARCH ON ANSWER ***************************/
+
 // https://leetcode.com/problems/first-bad-version/description/
 // https://leetcode.com/problems/sqrtx/
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 // https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
 // https://leetcode.com/problems/koko-eating-bananas/
-
-/************************ BINARY SEARCH ON ANSWER ***************************/
 
 public class BinarySearchOnAnswer {
 

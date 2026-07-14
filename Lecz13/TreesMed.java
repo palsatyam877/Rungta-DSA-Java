a70b3f495e28d701460cda05942d6078d8fa04ef
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-14 09:26:50 +0530

    Binary Trees Med

---
+++ Lecz13/TreesMed.java
@@ -0,0 +1,14 @@
+package Lecz13;
+
+// https://leetcode.com/problems/subtree-of-another-tree/
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+// https://leetcode.com/problems/binary-tree-right-side-view/
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/description/
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+public class TreesMed {
+    public static void main(String [] args) {
+        System.out.println("Hello Trwees");
+    }
+}

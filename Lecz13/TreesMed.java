ce0eed9e83caa465bf054308ad42acccfcfe7ce7
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-15 09:11:33 +0530

    Binary Trees Lec 14

--- Lecz13/TreesMed.java
+++ Lecz13/TreesMed.java
@@ -158,13 +158,6 @@ class Solution {
     }
 }
 */
-
-// https://leetcode.com/problems/count-good-nodes-in-binary-tree/description/
-// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
-// https://leetcode.com/problems/validate-binary-search-tree/
-// https://leetcode.com/problems/binary-tree-maximum-path-sum/
-// https://leetcode.com/problems/number-of-islands/
-
 import java.util.ArrayList;
 
 public class TreesMed {
---
+++ Lecz14/TreeContinue.java
@@ -0,0 +1,13 @@
+package Lecz14;
+
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/description/
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+// https://leetcode.com/problems/validate-binary-search-tree/
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// https://leetcode.com/problems/number-of-islands/
+
+public class TreeContinue {
+    public static void main(String [] args) {
+        System.out.println("Jai Maa Bhavani");
+    }
+}

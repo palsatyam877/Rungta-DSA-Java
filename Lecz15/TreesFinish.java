6f528ec81b0d55e377eecedb36a73b358fb1e823
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-16 09:37:31 +0530

    Binary Trees Lec 15

--- Lecz15/TreesFinish.java
+++ Lecz15/TreesFinish.java
@@ -3,7 +3,10 @@ package Lecz15;
 // https://leetcode.com/problems/validate-binary-search-tree/
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/
 // https://leetcode.com/problems/number-of-islands/
-
+// https://leetcode.com/problems/add-two-numbers/
 
 public class TreesFinish {
+    public static void main(String [] args) {
+        System.out.println("Hello");
+    }
 }

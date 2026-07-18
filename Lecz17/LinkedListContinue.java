5523eac8f785cb86204507a4cf76c5d3d3b77629
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-18 12:24:48 +0530

    Lecture 17 : 61. Rotate List -> Solved

--- Lecz17/LinkedListContinue.java
+++ Lecz17/LinkedListContinue.java
@@ -24,7 +24,62 @@ class Solution {
  */
 
 // https://leetcode.com/problems/linked-list-cycle-ii/
+/*
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> hs = new HashSet<>();
+
+        while(head != null) {
+            if(hs.contains(head))
+               return head;
+            hs.add(head);
+
+            head = head.next;
+        }
+
+        return null;
+    }
+}
+
+*/
+
 // https://leetcode.com/problems/rotate-list/
+
+/*
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k == 0)
+            return head;
+
+        ListNode tail = head , Thead = head;
+        int n = 1;
+
+        while(tail.next != null) {
+           tail = tail.next;
+           n++;
+        }
+
+        k = k % n;
+
+        if(k == 0)
+            return head;
+
+        k = n - k;
+
+        for(int i = 1; i < k; ++i) {
+            head = head.next;
+        }
+
+        ListNode T = head;
+        head = head.next;
+        T.next = null;
+        tail.next = Thead;
+
+        return head;
+    }
+}
+
+*/
 
 import java.util.ArrayList;
 import java.util.List;

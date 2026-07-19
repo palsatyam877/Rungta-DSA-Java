b1218eb20b5d813f7607c2bf7aad62f8aa5e8755
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-20 01:45:13 +0530

    Lecture 18 : Wrapping Up of LinkedList.

---
+++ Lecz18/LinkedListContinue.java
@@ -0,0 +1,21 @@
+package Lecz18;
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+// https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
+// https://leetcode.com/problems/copy-list-with-random-pointer/submissions/
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+import Lecz12.Trees;
+
+import java.util.ArrayDeque;
+import java.util.TreeMap;
+
+public class LinkedListContinue {
+    public static void main(String [] args) {
+        System.out.println("Jai Maa Bhavani");
+        TreeMap<Integer , Integer> x = new TreeMap<>();
+
+        int Cprefix = 0;
+
+        x.remove(Cprefix);
+    }
+}

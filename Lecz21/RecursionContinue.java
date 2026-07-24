9508cb54ff18695cf5d5fa6c7fc59a89bd8e4b73
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-24 10:13:32 +0530

    Lecture 21 : Solution update

--- Lecz21/RecursionContinue.java
+++ Lecz21/RecursionContinue.java
@@ -1,6 +1,36 @@
 package Lecz21;
 
 // https://www.geeksforgeeks.org/problems/reverse-a-stack/1
+
+/*
+class Solution {
+
+    public static void rev(Stack<Integer> st , Stack<Integer> Aux) {
+        if(Aux.size() == 0)
+            return;
+
+        int val = Aux.pop();
+
+        rev(st , Aux);
+
+        st.push(val);
+    }
+
+    public static void reverseStack(Stack<Integer> st) {
+        // code here
+        Stack<Integer> Aux = new Stack<>();
+
+        while(st.size() > 0) {
+            Aux.push(st.pop());
+        }
+
+        rev(st , Aux);
+
+    }
+}
+
+*/
+
 // https://www.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/1
 // https://leetcode.com/problems/combinations/submissions/1916018336/
 // https://leetcode.com/problems/permutations/submissions/1916710797/
@@ -9,9 +39,23 @@ package Lecz21;
 // https://leetcode.com/problems/longest-palindromic-substring/
 // https://leetcode.com/problems/longest-common-subsequence/submissions/1916275503/
 
+
+
 public class RecursionContinue {
 
+    static int fib(int n) {
+        if(n <= 1)
+            return n;
+        System.out.println("Started for : " + n);
+
+        int ans =  fib(n - 1) + fib(n - 2);
+
+        System.out.println("Ended for : " + n);
+
+        return ans;
+    }
+
     public static void main(String [] args) {
-        System.out.println("JAI MAA JAGAT JANANI");
+        System.out.println(fib(6));
     }
 }

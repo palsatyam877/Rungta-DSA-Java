29bf87572cad44e6e6ddb9114d525f8e9299b7ed
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-07-29 10:29:37 +0530

    Lecture 24 : LCS Recursive

--- Lecz24/RoadToDp.java
+++ Lecz24/RoadToDp.java
@@ -2,6 +2,33 @@ package Lecz24;
 
 /** Recursive Part is Remaining **/
 // https://leetcode.com/problems/longest-common-subsequence/
+/*
+class Solution {
+
+    int [][] dp;
+    public int f(int i , int j , String t1 , String t2) {
+            if(i < 0 || j < 0)
+                return 0;
+
+            if(dp[i][j] != -1)
+                 return dp[i][j];
+            if(t1.charAt(i) == t2.charAt(j))
+                return dp[i][j] =  1 + f(i - 1 , j - 1 , t1 , t2);
+            else
+                return dp[i][j] = Math.max(f(i - 1 , j , t1 , t2) , f(i , j - 1 , t1 , t2));
+    }
+
+    public int longestCommonSubsequence(String t1, String t2) {
+        dp = new int[t1.length() + 1][t2.length() + 1];
+
+        for(int i = 0; i <= t1.length(); ++i)
+           for(int j = 0; j <= t2.length(); ++j)
+               dp[i][j] = -1;
+
+        return f(t1.length() - 1 , t2.length() - 1 , t1 , t2);
+    }
+}
+*/
 
 // https://leetcode.com/problems/longest-palindromic-substring/
 
